f053b68b4985b08f6e41196a77b56f78e0b14af9
sami-p <sami.peterson14@gmail.com>
2021-08-10 10:07:57 -0500

    Drop unnecessary existing db, re-migrate

--- db/schema.rb
+++ db/schema.rb
@@ -32,42 +32,5 @@ ActiveRecord::Schema.define(version: 2021_08_10_134834) do
     t.index ["airline_id"], name: "index_flights_on_airline_id"
   end
 
-  create_table "gardens", force: :cascade do |t|
-    t.string "name"
-    t.boolean "organic"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "plant_plots", force: :cascade do |t|
-    t.bigint "plant_id"
-    t.bigint "plot_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["plant_id"], name: "index_plant_plots_on_plant_id"
-    t.index ["plot_id"], name: "index_plant_plots_on_plot_id"
-  end
-
-  create_table "plants", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "days_to_harvest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "plots", force: :cascade do |t|
-    t.integer "number"
-    t.string "size"
-    t.string "direction"
-    t.bigint "garden_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["garden_id"], name: "index_plots_on_garden_id"
-  end
-
   add_foreign_key "flights", "airlines"
-  add_foreign_key "plant_plots", "plants"
-  add_foreign_key "plant_plots", "plots"
-  add_foreign_key "plots", "gardens"
 end
