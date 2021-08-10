e19bb5c5e73132db7643aed1600f0208b14f30ab
sami-p <sami.peterson14@gmail.com>
2021-08-10 11:54:12 -0500

    Delete file after relocation of delete passenger

--- app/controllers/boarding_passengers_controller.rb
+++
@@ -1,8 +0,0 @@
-class BoardingPassengersController < ApplicationController
-
-  def destroy
-    @boarding_passenger = BoardingPassenger.find(params[:id])
-    @boarding_passenger.destroy
-    redirect_to "/flights"
-  end
-end
