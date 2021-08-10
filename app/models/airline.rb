9eb3b3960f464e0c0cefd769eed71a3a12064b7f
sami-p <sami.peterson14@gmail.com>
2021-08-10 11:52:17 -0500

    Build uniqe adults model action in airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,10 @@
 class Airline < ApplicationRecord
   has_many :flights
+
+  def unique_adults
+    self.flights.joins(:passengers)
+    .select('passengers.*')
+    .where('age >= 18')
+    .distinct
+  end
 end
