5d09abb5f8e1dc1628f57082c20e632c55ddc5fa
sami-p <sami.peterson14@gmail.com>
2021-08-10 11:09:52 -0500

    Update routes with boarding passengers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :flights
+  resources :flights do
+    resources :passengers, only: [:destroy]
+  end
+
+  resources :boarding_passengers
   resources :airlines
   resources :passengers
 end
